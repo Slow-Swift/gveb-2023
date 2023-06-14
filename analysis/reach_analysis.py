3fc9dd28175351707f54cad11abfbe9e5942526f
Finian Lugtigheid <55066633+Slow-Swift@users.noreply.github.com>
2023-06-14 14:09:46 -0700

    Did intitial reach calculations

---
+++ analysis/reach_analysis.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append('../') # This should probably be changed to a more sofisticated system at some point. i.e. install the package
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from data_wrangler import Dataset
+
+JUNCTION_FILE = '../processed_data/reach_junctions.csv'
+
+junctions = Dataset.load_file(
+    JUNCTION_FILE,
+    {
+        'id': int,
+        'crime_count': int,
+        'stores_count': int,
+        'transit_count': int,
+        'rtransit_count': int,
+        'schools_count': int,
+        'crime_reach': float,
+        'store_reach': float,
+        'transit_reach': float,
+        'rtransit_reach': float,
+        'schools_reach': float,
+    }
+)
+
+crime_reaches = [j['crime_reach'] for j in junctions]
+store_reaches = [j['store_reach'] for j in junctions]
+transit_reaches = [j['transit_reach'] for j in junctions]
+rtransit_reaches = [j['rtransit_reach'] for j in junctions]
+schools_reaches = [j['schools_reach'] for j in junctions]
+
+crime_counts = [j['crime_count'] for j in junctions]
+store_counts = [j['stores_count'] for j in junctions]
+transit_count = [j['transit_count'] for j in junctions]
+rtransit_count = [j['rtransit_count'] for j in junctions]
+schools_count = [j['schools_count'] for j in junctions]
+
+use_reach = True
+
+crime_data = crime_reaches if use_reach else crime_counts
+store_data = store_reaches if use_reach else store_counts
+transit_data = transit_reaches if use_reach else transit_count
+rtransit_data = rtransit_reaches if use_reach else rtransit_count
+schools_data = schools_reaches if use_reach else schools_count
+
+WIDTH = 2
+HEIGHT = 2
+
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.scatter(store_data, crime_data)
+plt.ylabel('Crime Reach')
+plt.xlabel('Store Reach')
+
+plt.subplot(2, 2, 2)
+plt.scatter(transit_data, crime_data)
+plt.ylabel('Crime Reach')
+plt.xlabel('Transit Reach')
+
+plt.subplot(2, 2, 3)
+plt.scatter(rtransit_data, crime_data)
+plt.ylabel('Crime Reach')
+plt.xlabel('Rapid Transit Reach')
+
+plt.subplot(2, 2, 4)
+plt.scatter(schools_data, crime_data)
+plt.ylabel('Crime Reach')
+plt.xlabel('Schools Reach')
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(store_data, transit_data, crime_data)
+ax.set_xlabel("Stores Reach")
+ax.set_ylabel("Transit Reach")
+ax.set_zlabel("Crime Reach")
+
+plt.show()
--- data_processing/reach_calculation.py
+++ data_processing/reach_calculation.py
@@ -1,12 +1,18 @@
 import sys
 sys.path.append('../') # This should probably be changed to a more sofisticated system at some point. i.e. install the package
 
+import math
+
 from heapq import heappush, heappop
 
 from ast import literal_eval
 from data_wrangler import Dataset
 
 JUNCTION_FILE = '../processed_data/junctions.csv'
+
+DISTANCE_SCALE = 0.0032
+STANDARD_DEVIATION = 200
+
 
 junctions = Dataset.load_file(JUNCTION_FILE)
 junctions.convert_properties({
@@ -19,7 +25,16 @@ junctions.convert_properties({
     'neighbors': lambda v : literal_eval(v) if v else []
 })
 
-def calculate_reach(junction, properties, dst_scale, limit):
+def normal_dst(distance, standard_deviation):
+    scale = 1 / (2 * math.pi * (standard_deviation ** 2))
+    power = distance ** 2 / (2 * standard_deviation ** 2)
+    distribution = math.exp(-power)
+    return scale * distribution
+
+def reach_dst(distance, scale):
+    return 1 / ((distance / scale + 1) ** 3)
+
+def calculate_reach(junction, properties, dst_func):
     """ Calculate a modified version of Borgatti's reach formula
     
     The range formula is:  sumweight * 1 / (dst_scale * dst + 1) ^ 2
@@ -32,7 +47,6 @@ def calculate_reach(junction, properties, dst_scale, limit):
         junction (Row): The junction to calculate the reach for
         prop (str): The property to use for junction weights
         dst_scale (float): The value to scale distance by. Should be in the range (0, 1]. Likely close to zero.
-        limit (float): The distance past which to stop calculations. This improves speed at the expense of a bit of accurracy.
 
     Returns:
         float: The calculated reach.
@@ -51,7 +65,7 @@ def calculate_reach(junction, properties, dst_scale, limit):
         # We square the denominator because this causes it to converge
         
         # Update the range values
-        scaled_dst = 1 / ((dst_scale * dst + 1) ** 2)
+        scaled_dst = dst_func(dst)
         for key in properties:
             reaches[key] += junctions[next_jun][properties[key]] * scaled_dst
               
@@ -61,15 +75,23 @@ def calculate_reach(junction, properties, dst_scale, limit):
             heappush(queue, (neighbor_dst, neighbor))
     return reaches
 
-def calculate_reaches(junctions, properties, dst_scale, limit):
+def calculate_reaches(junctions, properties, dst_func):
+    highest = { key: 0 for key in properties}
+    
     for i, junction in enumerate(junctions):
-        reaches = calculate_reach(junction, properties, dst_scale, limit)
+        reaches = calculate_reach(junction, properties, dst_func)
         for key in reaches:
             junction[key] = reaches[key]
+            highest[key] = max(highest[key], reaches[key])
         
         if (i+1) % 100 == 0:
             print(f'\rCalculated {i+1}/{len(junctions)}           ', end='')
     print(f'\rCalculated {len(junctions)}/{len(junctions)}        ')
+    print("Normalizing")
+    for junction in junctions:
+        for key in properties:
+            junction[key] /= highest[key]
+    print("Done")
     
 calculate_reaches(
     junctions, 
@@ -80,7 +102,6 @@ calculate_reaches(
         'rtransit_reach': 'rtransit_count',
         'schools_reach': 'schools_count'
     }, 
-    0.01, 
-    500
+    lambda dst: normal_dst(dst, STANDARD_DEVIATION)
 )
 junctions.write_to_file('../processed_data/reach_junctions.csv')
